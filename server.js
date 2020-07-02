03c69a33babdb9149e8b53288545d4c6c2fc61df
Rahul Tripathi <rahultripathi0401@gmail.com>
2020-07-02 11:41:34 +1000

    moving around for heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/templates'));
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '/templates/index.html');
 });
 
 app.listen(3000, () => console.log('Server started at port 3000'));
